37c7d71ea179dc49c4a7dd7d57514610dce06618
Oluwafemijohn1 <femiogundipe01@gmail.com>
2024-01-08 21:38:08 +0100

    logging aspect learning.

---
+++ src/main/java/demo/LoggingAspect.java
@@ -0,0 +1,27 @@
+package demo;
+
+
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    @Before("execution(* demo.ShoppingCart.checkout())")
+    public void beforeLogger(){
+        System.out.println("Before execution Loggers");
+    }
+
+    /**
+     * this means * for any return type
+     * then * for any package
+     * then .* for class
+     * then .checkout if there is any
+     * */
+    @After("execution(* *.*.checkout())")
+    public void afterLogger(){
+        System.out.println("After execution Loggers");
+    }
+}
